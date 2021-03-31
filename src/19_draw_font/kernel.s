b3c0a9968e0f53b2754978c2c26af0a05a81615d
NamikoToriyama <namiko.trym@gmail.com>
2021-04-01 01:21:18 +0900

    add 20

---
+++ src/19_draw_font/kernel.s
@@ -0,0 +1,120 @@
+; *******************************
+; マクロ
+; *******************************
+%include    "../include/define.s"
+%include    "../include/macro.s"
+
+    ORG     KERNEL_LOAD                     ; カーネルのロードアドレス
+[BITS 32]
+; *******************************
+; エントリポイント
+; *******************************
+kernel:
+    ; -------------------------------
+    ; フォントアドレスを取得する
+    ; -------------------------------
+    mov     esi, BOOT_LOAD + SECT_SIZE      ; ESI = 0x7c00 + 512
+    movzx   eax, word [esi + 0]
+    movzx   ebx, word [esi + 2]
+    shl     eax, 4                          ; shit left: eax << 4
+    add     eax, ebx
+    mov     [FONT_ADR], eax
+
+    ; -------------------------------
+    ; 文字の表示
+    ; -------------------------------
+    cdecl   draw_char, 0, 0, 0x010F, 'A'
+    cdecl   draw_char, 1, 0, 0x010F, 'B'
+    cdecl   draw_char, 2, 0, 0x010F, 'C'
+
+    ; -------------------------------
+    ; 8ビットの枠線
+    ; -------------------------------
+    mov     ah, 0x07                        ; 書き込みプレーンを指定 // 暗かったら0x0Fにする
+    mov     al, 0x02                        ; 書き込みプレーンを指定
+    mov     dx, 0x03C4                      ; シーケンサ制御ポート
+    out     dx, ax                          ; 出力
+
+    mov     [0x000A_0000 + 0], byte 0xFF
+
+    mov     ah, 0x04                        ; プレーン指定
+    out     dx, ax                          ; ポート出力
+
+    mov     [0x000A_0000 + 1], byte 0xFF    ; 8ドットの枠線
+
+    mov     ah, 0x02
+    out     dx, ax
+
+    mov     [0x000A_0000 + 2], byte 0xFF
+
+    mov     ah, 0x01
+    out     dx, ax
+
+    mov     [0x000A_0000 + 3], byte 0xFF
+
+    ;------------------------------------------
+    ; ２ドット目に画面を横切る横線
+    ;------------------------------------------
+    mov     ah, 0x02
+    out     dx, ax
+
+    lea     edi, [0x000A_0000]              ; edi = VRAMのアドレス
+    mov     ecx, 80                         ; ecx = 繰り返し回数
+    mov     al, 0xFF                        ; al = ビットパターン
+    rep     stosb                           ; *edi++=AL ????
+
+    ;------------------------------------------
+    ; 2行目に8ドットの矩形
+    ;------------------------------------------
+    mov     edi, 1                          ; edi = 行数
+
+    shl     edi, 8                          ; edi *= 256
+    lea     edi, [edi * 4 + edi + 0xA_0000] ; edi = VRAMアドレス
+
+    mov     [edi + (80 * 0)], word 0xFF
+    mov     [edi + (80 * 1)], word 0xFF
+    mov     [edi + (80 * 2)], word 0xFF
+    mov     [edi + (80 * 3)], word 0xFF
+    mov     [edi + (80 * 4)], word 0xFF
+    mov     [edi + (80 * 5)], word 0xFF
+    mov     [edi + (80 * 6)], word 0xFF
+    mov     [edi + (80 * 7)], word 0xFF
+
+    ;------------------------------------------
+    ; 3行目に文字を描画
+    ;------------------------------------------
+    mov     esi, 'A'
+    shl     esi, 4
+    add     esi, [FONT_ADR]
+
+    mov     edi, 2
+    shl     edi, 8
+    lea     edi, [edi * 4 + edi + 0xA_0000]
+
+    mov     ecx, 16                         ; ecx = 16; // 1文字の高さ
+
+    cdecl   draw_font, 63, 13               ; フォントの一覧表示
+
+.10L:
+        movsb
+        add edi, 80 - 1                     ; 1ドット分
+        loop .10L
+    ; -------------------------------
+    ; 処理の終了
+    ; -------------------------------    
+    jmp     $                   ;  while(1); // 無限ループ
+
+ALIGN 4, db 0
+FONT_ADR: dd 0
+
+;***********************************************************
+;  モジュール
+;***********************************************************
+%include    "../modules/protect/vga.s"
+%include    "../modules/protect/draw_char.s"
+%include    "../modules/protect/draw_font.s"
+
+;***********************************************************
+;  パディング（このファイルは8Kバイトとする）
+;***********************************************************
+    times   BOOT_SIZE - ($ - $$) db 0   ; 8kバイトを0で埋める
---
+++ src/modules/protect/draw_char.s
@@ -0,0 +1,88 @@
+draw_char:
+        ;-----------------------------------------
+        ; 【スタックフレームの構築】
+        ;-----------------------------------------
+        push ebp
+        mov ebp, esp
+
+        ;-----------------------------------------
+        ; 【レジスタの保存】
+        ;-----------------------------------------
+        push eax
+        push ebx
+        push ecx
+        push edx
+        push esi
+        push edi
+
+        ;-----------------------------------------
+        ; テストアンドセット
+        ;-----------------------------------------
+%ifdef	USE_TEST_AND_SET
+        cdecl test_and_set, IN_USE
+%endif
+
+        ;-----------------------------------------
+        ; コピー元フォントアドレスを設定
+        ;-----------------------------------------
+        movzx esi, byte [ebp + 20]                  ; CL = 文字コード
+        shl esi, 4                                  ; CL *= 16; // 1文字16バイト
+        add esi, [FONT_ADR]                         ; ESI = フォントアドレス
+
+        ;-----------------------------------------
+        ; コピー先アドレスを取得
+        ; Adr = 0xA0000 + (640 / 8 * 16) * y + x
+        ;-----------------------------------------
+        mov edi, [ebp + 12]                         ; Y (行)
+        shl edi, 8                                  ; EDI = Y * 256
+        lea edi, [edi * 4 + edi + 0xA0000]          ; EDI = Y * 4 + Y;
+        add edi, [ebp + 8]                          ; X (列)
+
+        ;-----------------------------------------
+        ; 1文字分のフォントを出力
+        ;-----------------------------------------
+        movzx ebx, word[ebp + 16]                   ; 表示色
+
+        cdecl vga_set_read_plane, 0x03              ; 書き込みプレーン: 輝度(I)
+        cdecl vga_set_write_plane, 0x08             ; 読み込みプレーン: 輝度(II)
+        cdecl vram_font_copy, esi, edi, 0x08, ebx
+
+        cdecl vga_set_read_plane, 0x02              ; 書き込みプレーン: 赤(R)
+        cdecl vga_set_write_plane, 0x04             ; 読み込みプレーン: 赤(R)
+        cdecl vram_font_copy, esi, edi, 0x04, ebx
+
+        cdecl vga_set_read_plane, 0x01              ; 緑
+        cdecl vga_set_write_plane, 0x02
+        cdecl vram_font_copy, esi, edi, 0x02, ebx
+
+        cdecl vga_set_read_plane, 0x00              ; 青
+        cdecl vga_set_write_plane, 0x01
+        cdecl vram_font_copy, esi, edi, 0x01, ebx
+
+        ;-----------------------------------------
+        ; 変数のクリア
+        ;-----------------------------------------
+%ifdef	USE_TEST_AND_SET
+        mov [IN_USE], dword 0
+%endif
+
+        ;-----------------------------------------
+        ; 【レジスタの復帰】
+        ;-----------------------------------------
+        pop edi
+        pop esi
+        pop edx
+        pop ecx
+        pop ebx
+        pop eax
+
+        ;-----------------------------------------
+        ; 【スタックフレームの破棄】
+        ;-----------------------------------------
+        mov esp, ebp
+        pop ebp
+
+        ret
+
+ALIGN 4, db 0
+IN_USE:	dd 0
