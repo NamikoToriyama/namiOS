417c0f1b2c76a8116553e2213bae875c1cddbc40
NamikoToriyama <namiko.trym@gmail.com>
2021-04-01 17:33:58 +0900

    add 24&25

--- src/23_draw_line/kernel.s
+++ src/23_draw_line/kernel.s
@@ -29,26 +29,27 @@ kernel:
     cdecl   draw_color_bar, 63, 4           ; カラーバーの表示
 
     ;------------------------------------------
-    ; ピクセルの描画
+    ; 線を描画
     ;------------------------------------------
-    cdecl draw_pixel, 8, 4, 0x01                ; 青
-    cdecl draw_pixel, 9, 5, 0x01
-    cdecl draw_pixel, 10, 6, 0x02               ; 緑
-    cdecl draw_pixel, 11, 7, 0x02
-    cdecl draw_pixel, 12, 8, 0x03
-    cdecl draw_pixel, 13, 9, 0x03
-    cdecl draw_pixel, 14, 10, 0x04              ; 赤
-    cdecl draw_pixel, 15, 11, 0x04
+    cdecl draw_line, 100, 100, 0, 0, 0x0F
+    cdecl draw_line, 100, 100, 200, 0, 0x0F
+    cdecl draw_line, 100, 100, 200, 200, 0x0F
+    cdecl draw_line, 100, 100, 0, 200, 0x0F   ; 白？
 
-    cdecl draw_pixel, 15, 4, 0x03
-    cdecl draw_pixel, 14, 5, 0x03
-    cdecl draw_pixel, 13, 6, 0x04
-    cdecl draw_pixel, 12, 7, 0x04
-    cdecl draw_pixel, 11, 8, 0x01
-    cdecl draw_pixel, 10, 9, 0x01
-    cdecl draw_pixel, 9, 10, 0x02
-    cdecl draw_pixel, 8, 11, 0x02
+    cdecl draw_line, 100, 100, 50, 0, 0x02    ; 緑
+    cdecl draw_line, 100, 100, 150, 0, 0x03   ; 青
+    cdecl draw_line, 100, 100, 150, 200, 0x04 ; 赤
+    cdecl draw_line, 100, 100, 50, 200, 0x05
 
+    cdecl draw_line, 100, 100, 0, 50, 0x02
+    cdecl draw_line, 100, 100, 200, 50, 0x03
+    cdecl draw_line, 100, 100, 200, 150, 0x04
+    cdecl draw_line, 100, 100, 0, 150, 0x05
+
+    cdecl draw_line, 100, 100, 100, 0, 0x0F
+    cdecl draw_line, 100, 100, 200, 100, 0x0F
+    cdecl draw_line, 100, 100, 100, 200, 0x0F
+    cdecl draw_line, 100, 100, 0, 100, 0x0F
 .10L:
         movsb
         add edi, 80 - 1                     ; 1ドット分
@@ -73,6 +74,7 @@ FONT_ADR: dd 0
 %include    "../modules/protect/draw_str.s"
 %include    "../modules/protect/draw_color_bar.s"
 %include    "../modules/protect/draw_pixel.s"
+%include    "../modules/protect/draw_line.s"
 
 ;***********************************************************
 ;  パディング（このファイルは8Kバイトとする）
--- src/24_draw_rect/kernel.s
+++ src/24_draw_rect/kernel.s
@@ -29,25 +29,13 @@ kernel:
     cdecl   draw_color_bar, 63, 4           ; カラーバーの表示
 
     ;------------------------------------------
-    ; ピクセルの描画
+    ; 線を描画
     ;------------------------------------------
-    cdecl draw_pixel, 8, 4, 0x01                ; 青
-    cdecl draw_pixel, 9, 5, 0x01
-    cdecl draw_pixel, 10, 6, 0x02               ; 緑
-    cdecl draw_pixel, 11, 7, 0x02
-    cdecl draw_pixel, 12, 8, 0x03
-    cdecl draw_pixel, 13, 9, 0x03
-    cdecl draw_pixel, 14, 10, 0x04              ; 赤
-    cdecl draw_pixel, 15, 11, 0x04
+    cdecl draw_rect, 100, 100, 200, 200, 0x02     ; 緑
+    cdecl draw_rect, 400, 400, 200, 200, 0x03   ; 青
+    cdecl draw_rect, 300, 150, 200, 300, 0x04 ; 赤
+    cdecl draw_rect, 100, 400, 0, 200, 0x0F   ; 白？
 
-    cdecl draw_pixel, 15, 4, 0x03
-    cdecl draw_pixel, 14, 5, 0x03
-    cdecl draw_pixel, 13, 6, 0x04
-    cdecl draw_pixel, 12, 7, 0x04
-    cdecl draw_pixel, 11, 8, 0x01
-    cdecl draw_pixel, 10, 9, 0x01
-    cdecl draw_pixel, 9, 10, 0x02
-    cdecl draw_pixel, 8, 11, 0x02
 
 .10L:
         movsb
@@ -73,6 +61,8 @@ FONT_ADR: dd 0
 %include    "../modules/protect/draw_str.s"
 %include    "../modules/protect/draw_color_bar.s"
 %include    "../modules/protect/draw_pixel.s"
+%include    "../modules/protect/draw_line.s"
+%include    "../modules/protect/draw_rect.s"
 
 ;***********************************************************
 ;  パディング（このファイルは8Kバイトとする）
